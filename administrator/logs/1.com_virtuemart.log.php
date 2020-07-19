c54c6abae02abea15558b6d016517b873b32ae68
JiXU Luo <goodluck1031@outlook.com>
2020-07-19 21:36:22 +0800

    Initial commit

---
+++ administrator/logs/1.com_virtuemart.log.php
@@ -0,0 +1,4 @@
+#
+#<?php die("Forbidden."); ?>
+
+2020-06-19 06:30:07 ERROR vmError: Warning, the Safe Path is not configured yet. Use this link to the &lt;a href='http://localhost/joomlaville/administrator/index.php?option=com_virtuemart&amp;view=updatesmigration&amp;show_spwizard=1' &gt;setup wizard&lt;/a&gt;
---
+++ administrator/logs/error.php
@@ -0,0 +1,12 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2020-07-18 17:49:34 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2020-07-18T17:49:34+00:00	INFO 127.0.0.1	joomlafailure	Username and password do not match or you do not have an account yet.
+2020-07-18T17:49:58+00:00	INFO 127.0.0.1	joomlafailure	Username and password do not match or you do not have an account yet.
+2020-07-18T17:50:10+00:00	INFO 127.0.0.1	joomlafailure	Username and password do not match or you do not have an account yet.
+2020-07-18T17:50:44+00:00	INFO 127.0.0.1	joomlafailure	Username and password do not match or you do not have an account yet.
+2020-07-18T17:50:52+00:00	INFO 127.0.0.1	joomlafailure	Username and password do not match or you do not have an account yet.
+2020-07-18T17:51:02+00:00	INFO 127.0.0.1	joomlafailure	Username and password do not match or you do not have an account yet.
